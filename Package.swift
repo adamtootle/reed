9cf00ce65aa6fe965bb638a71613ce0c349e42b1
Adam Tootle <adamtootle@gmail.com>
2026-05-09 01:19:59 -0400

    chore: add Hummingbird and ArgumentParser dependencies

--- Package.swift
+++ Package.swift
@@ -1,22 +1,26 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "reed",
+    platforms: [.macOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .executable(
-            name: "reed",
-            targets: ["reed"]
-        )
+        .executable(name: "reed", targets: ["reed"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/hummingbird-project/hummingbird", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "reed"
+        .executableTarget(
+            name: "reed",
+            dependencies: [
+                .product(name: "Hummingbird", package: "hummingbird"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ],
+            resources: [
+                .copy("Resources")
+            ]
         ),
         .testTarget(
             name: "reedTests",
